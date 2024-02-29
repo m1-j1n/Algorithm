fdcfde464ce0a73d1629a63af4d88cc7e921430f
m1-j1n <152361302+m1-j1n@users.noreply.github.com>
2024-02-29 23:15:26 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -0,0 +1,31 @@
+# def solution(s):
+#     answer = ''
+#     index = 0
+    
+#     for i in s:
+#         if s == ' ':
+#             answer += ' '
+#             index = 0
+#         else:
+#             if index % 2 == 0:
+#                 answer += i.upper()
+#             else:
+#                 answer += i.lower()
+#             index += 1
+            
+#     return answer
+
+def solution(s):
+    answer = ''
+    index = 0
+    for spell in s:
+        if spell == ' ':
+            answer += ' '
+            index = 0
+        else:
+            if index % 2 == 0:
+                answer += spell.upper()
+            else:
+                answer += spell.lower()
+            index += 1
+    return answer
