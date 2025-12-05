74da63b98976fcc049f0cea4d2b2f1d11289c38c
m1-j1n <152361302+m1-j1n@users.noreply.github.com>
2025-12-05 13:09:36 +0900

    [Silver IV] Title: ATM, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.py
+++ 백준/Silver/11399. ATM/ATM.py
@@ -1,29 +1,13 @@
 n = int(input())
-A = list(map(int, input().split()))
-S = [0] * n
+arr = list(map(int, input().split()))
 
-# 삽입정렬
-for i in range(1,n):
-    insert_point = i # 현재 인덱스 값
-    insert_value = A[i]
-    for j in range(i-1, -1, -1):
-        if A[j] < A[i]:
-            insert_point = j+1
-            break
-        if j==0:
-            insert_point = 0
-    
-    for j in range(i, insert_point, -1):
-        A[j] = A[j-1]
-    
-    A[insert_point] = insert_value
+arr.sort()
 
-S[0] = A[0]
-for i in range(1,n): 
-    S[i] = S[i-1] + A[i] # 구간합 공식
-    
-sum = 0
-for i in range(n):
-    sum += S[i]
+time_sum = 0
+answer = 0
 
-print(sum)
+for t in arr:
+    time_sum += t
+    answer += time_sum
+
+print(answer)
